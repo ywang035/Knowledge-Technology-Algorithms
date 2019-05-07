56a2542ed7edc52ad6e8c0ae833938c7591aa7b0
Y. WANG <49180491+ywang035@users.noreply.github.com>
2019-05-07 20:13:41 +1000

    Add files via upload

---
+++ tester.java
@@ -0,0 +1,12 @@
+package Default;
+
+public class tester {
+
+    public static void main(String[] args){
+
+        String str = "today";
+        String substr = str.substring(str.length()-2);
+
+        System.out.println(substr);
+    }
+}
